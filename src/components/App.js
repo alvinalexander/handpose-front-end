0a7638452a30b54b206a823fcb84584bcffa982b
Alvin Ghouas <aghouas@adobe.com>
2020-05-16 14:40:06 -0700

    Add VideoFrame component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import VideoFrame from './VideoFrame';
 
 function App() {
   return (
     <div className="App">
-      App
+      <h1>Tensorflow object detection</h1>
+      <VideoFrame />
     </div>
   );
 }
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import App from './App'
 
-const Root = ({}) => (<App/>);
+const Root = () => (<App/>);
 
 export default Root;
